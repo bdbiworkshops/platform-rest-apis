264851b2555c3e520c1a5c25f565c6f3ee084ec6
Big Data Big Impact Workshops <143761123+bdbiworkshops@users.noreply.github.com>
2023-09-19 19:42:44 -0400

    Implement app.py

--- app.py
+++ app.py
@@ -10,15 +10,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.app_context().push()
 db = SQLAlchemy(app)
 
-# TODO: define columns
-
 class Account(db.Model):
-    pass
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(80), primary_key=True, unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __init__(self, name, username, email):
+        self.name = name
+        self.username = username
+        self.email = email
+
 
 db.create_all()
 
 
-# TODO: write routes
+@app.route('/accounts/<username>', methods=['GET'])
+def get_account(username):
+    account = Account.query.get(username)
+    del account.__dict__['_sa_instance_state']
+    return jsonify(account.__dict__)
+
+
+@app.route('/accounts/', methods=['POST'])
+def create_account():
+  body = request.get_json()
+  db.session.add(Account(body['name'], body['username'], body['email']))
+  db.session.commit()
+  return "account created"
+
+
+@app.route('/accounts/<username>', methods=['DELETE'])
+def delete_account(username):
+    db.session.query(Account).filter_by(username=username).delete()
+    db.session.commit()
+    return "account deleted"
 
      
 # Run app
